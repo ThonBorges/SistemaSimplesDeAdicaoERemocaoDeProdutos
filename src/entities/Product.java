ece66b45763668fbbaea26bbf2e7ddf6dc86f57d
ThonBorges <96311881+ThonBorges@users.noreply.github.com>
2024-05-27 16:13:21 -0300

    Update Product.java

--- src/entities/Product.java
+++ src/entities/Product.java
@@ -6,6 +6,12 @@ public class Product {
     public double preco;
     public int quantidade;
 
+    public Product (String nome, double preco, int quantidade) {
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+
     public double valorTotalEstoque() {
         return preco * quantidade;
     }
